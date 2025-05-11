c182df17aa8d4393614811fc54d1e3f8bd81af3c
周伯川 <zbc200403184552@qq.com>
2025-05-11 16:15:39 +0800

    通过CommentService 实现类实现了 ICommentService 接口，从而实现通过id对评论进行查找删除

--- Backend/TaskManage/Application/Services/CommentService.cs
+++ Backend/TaskManage/Application/Services/CommentService.cs
@@ -1,7 +1,7 @@
 using Application.Interfaces;
 using Domain.Entities;
 using Domain.Repository;
-using System.Collections.Generic;
+using System;
 using System.Threading.Tasks;
 
 namespace Application.Services
@@ -18,7 +18,6 @@ namespace Application.Services
         // 添加评论
         public async Task AddCommentAsync(Comment comment)
         {
-            // 可以在这里进行一些验证，比如评论内容不能为空
             if (string.IsNullOrWhiteSpace(comment.Content))
             {
                 throw new ArgumentException("评论内容不能为空");
@@ -27,33 +26,28 @@ namespace Application.Services
             await _commentRepository.AddAsync(comment);
         }
 
-        // 根据任务ID获取评论列表
-        public async Task<List<Comment>> GetCommentsByTaskIdAsync(int taskId)
+        // 根据评论ID获取评论
+        public async Task<Comment> GetCommentByIdAsync(int id)
         {
-            // 可以根据任务ID查询评论
-            return await _commentRepository.GetByTaskIdAsync(taskId);
+            var comment = await _commentRepository.GetByIdAsync(id);
+            if (comment == null)
+            {
+                throw new ArgumentException("未找到该评论");
+            }
+
+            return comment;
         }
 
-        // 根据用户ID获取评论列表
-        public async Task<List<Comment>> GetCommentsByUserIdAsync(int userId)
+        // 根据评论ID删除评论
+        public async Task DeleteCommentAsync(int id)
         {
-            // 根据用户ID查询评论
-            return await _commentRepository.GetByUserIdAsync(userId);
-        }
-
-        // 删除评论
-        public async Task DeleteCommentAsync(int commentId)
-        {
-            // 获取评论
-            var comment = await _commentRepository.GetByIdAsync(commentId);
-
+            var comment = await _commentRepository.GetByIdAsync(id);
             if (comment == null)
             {
                 throw new ArgumentException("评论未找到");
             }
 
-            // 删除评论
-            await _commentRepository.DeleteByIdAsync(commentId);
+            await _commentRepository.DeleteByIdAsync(id);
         }
     }
 }
