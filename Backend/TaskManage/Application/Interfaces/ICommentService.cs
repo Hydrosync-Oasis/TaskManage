36e9ff90bfa43f4daafec398f8524ffb53a34af3
周伯川 <zbc200403184552@qq.com>
2025-05-11 16:11:18 +0800

    定义仅通过 Comment 的 Id 操作的接口

--- Backend/TaskManage/Application/Interfaces/ICommentService.cs
+++ Backend/TaskManage/Application/Interfaces/ICommentService.cs
@@ -4,9 +4,9 @@ namespace Application.Interfaces
 {
     public interface ICommentService
     {
-        Task AddCommentAsync(Comment comment);
-        Task<List<Comment>> GetCommentsByTaskIdAsync(int taskId);
-        Task<List<Comment>> GetCommentsByUserIdAsync(int userId);
-        Task DeleteCommentAsync(int commentId);
+        Task AddCommentAsync(Comment comment);         // 添加评论（仍需 Comment 对象）
+        Task<Comment> GetCommentByIdAsync(int id);     // 通过评论ID获取评论
+        Task DeleteCommentAsync(int id);               // 通过评论ID删除评论
     }
 }
+
