fb35490dfe4866f476939d7f06ff83d62e3c3043
尹义坤 <3014044095@qq.com>
2025-05-18 13:53:59 +0800

    实现project控制器

--- Backend/TaskManage/Application/DTOs/UserDto.cs
+++ Backend/TaskManage/Application/DTOs/UserDto.cs
@@ -6,6 +6,21 @@ using System.Threading.Tasks;
 
 namespace Application.DTOs
 {
+        // 注册请求 DTO
+    public class RegisterDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+    
+    // 登录请求 DTO
+    public class LoginDto
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+
+    // 用户信息 DTO
     public class UserDto
     {
         public int Id { get; set; }
@@ -14,6 +29,7 @@ namespace Application.DTOs
         public string? AvatarUrl { get; set; }
     }
 
+     // 登录结果 DTO（包含 token 和用户信息）
     public class LoginResultDto
     {
         public string Token { get; set; } = null!;
