c38570c723b131e5ff60bfad85b5baf5c6808ea7
尹义坤 <3014044095@qq.com>
2025-05-21 17:42:05 +0800

    注入 Service 到依赖容器中

--- Backend/TaskManage/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
+++ Backend/TaskManage/Infrastructure/DependencyInjection/ServiceCollectionExtensions.cs
@@ -6,9 +6,12 @@ using Infrastructure.EntityConfiguration;
 using Infrastructure.Repository;
 using Microsoft.Extensions.DependencyInjection;
 
+
 namespace Infrastructure.DependencyInjection {
-    public static class ServiceCollectionExtensions {
-        public static IServiceCollection AddApplicationServices(this IServiceCollection services) {
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
             // 注册服务
             services.AddScoped<IUserService, UserService>();
             services.AddScoped<ITaskService, TaskService>();
@@ -16,7 +19,8 @@ namespace Infrastructure.DependencyInjection {
             return services;
         }
 
-        public static IServiceCollection AddRepositoryServices(this IServiceCollection services) {
+        public static IServiceCollection AddRepositoryServices(this IServiceCollection services)
+        {
             // 注册仓储
             services.AddScoped<IUserRepository, UserRepository>();
             services.AddScoped<ITaskRepository, TaskRepository>();
@@ -25,9 +29,17 @@ namespace Infrastructure.DependencyInjection {
             return services;
         }
 
-        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services) {
+        public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+        {
             services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
             return services;
         }
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services)
+        {
+            services.AddScoped<IProjectService, ProjectService>();
+        // 其他服务注册...
+            return services;
+        }
+
     }
 }
