ce5729ccad2b5d479248b0c404f54cb8d00c9689
张航 <2091626994@qq.com>
2025-05-24 17:57:39 +0800

    创建了全局错误处理服务(errorHandler.js) 改进了 axios 响应拦截器 创建了通用错误显示组件(ErrorHandler.vue)

--- Frontend/task-manage/src/utils/axios.js
+++ Frontend/task-manage/src/utils/axios.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import errorHandler from './errorHandler'
 
 // 创建 axios 实例
 const service = axios.create({
@@ -21,4 +22,18 @@ service.interceptors.request.use(
   }
 )
 
-export default service 
+// 响应拦截器
+service.interceptors.response.use(
+  response => {
+    return response
+  },
+  error => {
+    // 使用错误处理服务处理错误
+    errorHandler.handleApiError(error)
+    
+    // 返回 Promise.reject 以便调用方可以进一步处理错误
+    return Promise.reject(error)
+  }
+)
+
+export default service
