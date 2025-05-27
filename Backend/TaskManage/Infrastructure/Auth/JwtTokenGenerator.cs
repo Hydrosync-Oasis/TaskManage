e3008c6b2c99be049d6604b42fa806cfe688e531
尹义坤 <3014044095@qq.com>
2025-05-27 17:56:14 +0800

    把添加project改为只有项目管理员才能实现

--- Backend/TaskManage/Infrastructure/Auth/JwtTokenGenerator.cs
+++ Backend/TaskManage/Infrastructure/Auth/JwtTokenGenerator.cs
@@ -5,26 +5,33 @@ using System.Security.Claims;
 using System.Text;
 using Domain.Repository;
 using Application.Interfaces;
+using Domain.Entities;
 
 namespace Infrastructure.Auth
 {
-    public class JwtTokenGenerator(IConfiguration configuration, IUserRepository userRepository) : IJwtTokenGenerator {
-        public async Task<string> GenerateToken(int userId) {
-            if ((await userRepository.GetUserByIdAsync(userId)) is null) {
+    public class JwtTokenGenerator(IConfiguration configuration, IUserRepository userRepository) : IJwtTokenGenerator
+    {
+        public async Task<string> GenerateToken(int userId)
+        {
+            // 获取用户信息
+            var user = await userRepository.GetUserByIdAsync(userId);
+            if (user == null)
                 throw new ArgumentException("不存在该用户");
-            }
 
+            // 密钥和签名
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["Jwt:Key"]!));
             var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
 
-            var claims = new[]
+            // 构建 Claims，包括角色
+            var claims = new List<Claim>
             {
-                new Claim(ClaimTypes.NameIdentifier, userId.ToString()),
-                new Claim(ClaimTypes.Name, (await userRepository.GetUserByIdAsync(userId))!.UserName),
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.UserName),
                 new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
-                new Claim(ClaimTypes.Role,(await userRepository.GetUserRoleByIdAsync(userId)).ToString()!)
+                new Claim(ClaimTypes.Role, user.UserRole.ToString()) // 关键角色信息
             };
 
+            // 构建 Token
             var token = new JwtSecurityToken(
                 issuer: configuration["Jwt:Issuer"],
                 audience: configuration["Jwt:Audience"],
