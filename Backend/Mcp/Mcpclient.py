4d8319ea277e0158981ed94df0f76b677a5139ce
TinFeng444 <19903778945@163.com>
2025-05-28 13:28:49 +0800

    在client中添加了输入token会得出用户id代码，并能将token和用户id传输到server中

--- Backend/Mcp/Mcpclient.py
+++ Backend/Mcp/Mcpclient.py
@@ -31,7 +31,7 @@ class MCPClient:
         self.session: Optional[ClientSession] = None
         self.exit_stack = exit_stack if exit_stack else AsyncExitStack()
         self.llm = ChatOpenAI(
-            api_key="sk-c78e054c",
+            api_key="**************",
             base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
             model="qwen-max"
         )
