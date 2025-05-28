414b566479c5db478f7e0a520f722483653598ba
马新懿 <conanmarisa@qq.com>
2025-05-28 22:19:12 +0800

    修复Dto映射，无评论不提醒

--- Backend/TaskManage/Application/DTOMappings/TaskMapping.cs
+++ Backend/TaskManage/Application/DTOMappings/TaskMapping.cs
@@ -6,7 +6,8 @@ namespace Application.DTOMappings {
     public class TaskMapping {
         public static void Register(TypeAdapterConfig config) {
             config.NewConfig<TaskNode, TaskDto>()
-                .Map(x => x.DependencyTaskIds, y => y.DependentNodes.Select(z => z.Id));
+                .Map(x => x.DependencyTaskIds, y => y.DependentNodes.Select(z => z.Id))
+                .Map(x => x.Status, y => y.TaskStatus);
         }
     }
 }
